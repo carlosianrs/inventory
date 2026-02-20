505ab39e25407de901f184484eb793b7d6524b47
Carlos Ian Rodrigues dos Santos <carlosian894@gmail.com>
2026-02-19 22:10:43 -0300

    :bug: fix: Ajuste com a captura de erros com api

--- front/http/api/api-client.ts
+++ front/http/api/api-client.ts
@@ -1,8 +1,7 @@
 'use server'
 
-import { serverConfig } from "@/front/lib/settings";
+import { serverConfig } from "@/lib/settings";
 import axios from "axios";
-import { cookies } from "next/headers";
 
 export type ResponseError = {
   success: boolean;
@@ -14,13 +13,6 @@ export const api = axios.create({
   timeout: 30000,
 })
 
-api.interceptors.request.use(async config => {
-  const cookiesStore = await cookies();
-  const token = cookiesStore.get(serverConfig.TOKEN)?.value;
-  if (token) config.headers.Authorization = `Bearer ${token}`
-  return config;
-})
-
 api.interceptors.response.use(
   response => response,
   (error) => {
@@ -30,11 +22,11 @@ api.interceptors.response.use(
     } else if (error.message === 'Network Error') {
       message = 'Conexão instável, verifique a sua internet'
     } else if (!error.response) {
-      message = `Erro desconhecido: ${error.message}`
+      message = `Erro desconhecido: ${error.message || error?.code || error?.name}`
     }
 
     if (message) error.message = message;
-    else if (error.response?.data?.message) error.message = error.response.data.message
+    else if (error.response?.data?.message || error.response?.data?.error) error.message = error.response.data.message || error.response?.data?.error
 
     return Promise.reject(error)
   }
